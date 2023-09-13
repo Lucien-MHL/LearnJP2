b3c92a20331ef6cf9c752a11bfd3b1362a087d18
Lucien <lucienengineerlife@gmail.com>
2023-09-13 20:55:53 +0800

    add: GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { store } from './redux/store.jsx'
 import { Provider } from 'react-redux'
+import { GlobalStyle, Palette } from './theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <GlobalStyle theme={Palette} />
       <App />
     </Provider>
   </React.StrictMode>
---
+++ src/theme.jsx
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    color: ${({ theme }) => theme.mercury};
+    background-color: ${({ theme }) => theme.pickled_blue_wood};
+    text-rendering: optimizeLegibility;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const Palette = {
+  mine_shaft: '#242424',
+  mercury: '#e3e3e3',
+  pickled_blue_wood: '#2F464F',
+  black: '#000000',
+  green: '#77f779',
+  red: '#f52727',
+}
