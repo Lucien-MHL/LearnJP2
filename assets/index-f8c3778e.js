afba5df8f49248f8e965e1b3a7ca073569d0324c
Lucien-MHL <Lucien-MHL@users.noreply.github.com>
2023-09-27 09:27:24 +0000

    deploy: fbce9162cdca3a198fa25903221360b01fa789dc

--- assets/index-f8c3778e.js
+++ assets/index-f8c3778e.js
@@ -1,22 +1,22 @@
-import{s as i,L as a,j as n}from"./index-c75df100.js";import{u as o}from"./useTranslation-58c8887c.js";const t={Container:i.div`
+import{s as t,L as a,j as n}from"./index-4dfbe26a.js";import{u as r,l as s}from"./linkData-b800f283.js";const i={Container:t.div`
     height: 100%;
     margin: auto;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     color: ${({theme:e})=>e.mine_shaft};
-  `,Header:i.header`
+  `,Header:t.header`
     margin-top: 60px;
     text-align: center;
     user-select: none;
-  `,Main:i.p`
+  `,Main:t.p`
     font-size: 4.5rem;
-  `,Sub:i.p`
+  `,Sub:t.p`
     font-size: 3.15rem;
-  `,ListSection:i.section`
+  `,ListSection:t.section`
     display: flex;
     margin: auto;
-  `,LinkButton:i(a)`
+  `,LinkButton:t(a)`
     position: relative;
     color: ${({theme:e})=>e.mine_shaft};
     font-size: 1.75rem;
@@ -55,4 +55,4 @@ import{s as i,L as a,j as n}from"./index-c75df100.js";import{u as o}from"./useTr
     &:hover {
       translate: 0 -10px;
     }
-  `};function l(){const{t:e}=o();return n.jsxs(t.Container,{children:[n.jsxs(t.Header,{children:[n.jsx(t.Main,{children:e("homepage_title.main")}),n.jsx(t.Sub,{children:e("homepage_title.sub")})]}),n.jsxs(t.ListSection,{children:[n.jsx(t.LinkButton,{children:e("hiragana")}),n.jsx(t.LinkButton,{children:e("katakana")}),n.jsx(t.LinkButton,{children:e("hiraganaDakuon")}),n.jsx(t.LinkButton,{children:e("katakanaDakuon")})]})]})}export{l as default};
+  `};function d(){const{t:e}=r();return n.jsxs(i.Container,{children:[n.jsxs(i.Header,{children:[n.jsx(i.Main,{children:e("homepage_title.main")}),n.jsx(i.Sub,{children:e("homepage_title.sub")})]}),n.jsx(i.ListSection,{children:s().map(o=>n.jsx(i.LinkButton,{to:o.link,children:e(o.key)},o.key))})]})}export{d as default};
