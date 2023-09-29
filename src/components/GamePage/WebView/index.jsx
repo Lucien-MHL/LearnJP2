152ada07e1c1ef3e47220a21730a8d23fa00b071
Lucien <lucienengineerlife@gmail.com>
2023-09-29 23:54:47 +0800

    feat: gamePage webview UI

--- src/components/GamePage/WebView/index.jsx
+++ src/components/GamePage/WebView/index.jsx
@@ -1,3 +1,26 @@
+import { S } from './styles'
+import { Home } from '../../../assets/icons'
+import { useTranslation } from 'react-i18next'
+
 export default function WebView() {
-  return <h1>WebView</h1>
+  const { t } = useTranslation()
+  return (
+    <S.Container>
+      <S.HomeIcon to='/'>
+        <Home size={'100%'} />
+      </S.HomeIcon>
+      <S.QuestionSection>
+        <S.Subject>シ</S.Subject>
+      </S.QuestionSection>
+      <S.InputSection>
+        <S.Blackboard>
+          <S.NumberOfQuestion>
+            <S.Current>{t('current', { current: 1 })}</S.Current>
+            <S.Total>{t('total', { total: 46 })} </S.Total>
+          </S.NumberOfQuestion>
+          <S.Input type='text' placeholder='輸入答案' />
+        </S.Blackboard>
+      </S.InputSection>
+    </S.Container>
+  )
 }
