c52c3b728737a4e22b694b767b968bffba102883
Lucien <lucienengineerlife@gmail.com>
2023-10-04 00:51:15 +0800

    feat: random subject & next subject

---
+++ src/components/GamePage/MobileView/Option/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const S = {
+  Option: styled.p`
+    font-size: 1.5rem;
+    text-align: center;
+    padding: 0.75rem;
+    margin: 0.5rem;
+    width: 60px;
+  `,
+}
--- src/components/GamePage/WebView/index.jsx
+++ src/components/GamePage/WebView/index.jsx
@@ -1,11 +1,18 @@
 import { S } from './styles'
-import { Home } from '../../../assets/icons'
 import { useTranslation } from 'react-i18next'
+import { useSelector, useDispatch } from 'react-redux'
+import { Home } from '../../../assets/icons'
 import { useDevice } from '../../../hooks'
+import { selectById, nextSubject } from '../../../redux/slices/questionSlice'
 
 export default function WebView() {
   const { t } = useTranslation()
   const device = useDevice()
+  const dispatch = useDispatch()
+  const current = useSelector(state => selectById(state, 'current'))
+  const total = useSelector(state => selectById(state, 'total'))
+  const count = useSelector(state => selectById(state, 'count'))
+
   return (
     <>
       {device.isResize() ? (
@@ -20,15 +27,20 @@ export default function WebView() {
             <Home size={'100%'} />
           </S.HomeIcon>
           <S.QuestionSection>
-            <S.Subject>シ</S.Subject>
+            <S.Subject>{current.word}</S.Subject>
           </S.QuestionSection>
-          <S.InputSection>
+          <S.InputSection
+            onSubmit={e => {
+              e.preventDefault()
+              dispatch(nextSubject(current.id))
+            }}
+          >
             <S.Blackboard>
               <S.NumberOfQuestion>
-                <S.Current>{t('current', { current: 1 })}</S.Current>
-                <S.Total>{t('total', { total: 46 })} </S.Total>
+                <S.Current>{t('current', { current: count })}</S.Current>
+                <S.Total>{t('total', { total: total })} </S.Total>
               </S.NumberOfQuestion>
-              <S.Input type='text' placeholder='輸入答案' />
+              <S.Input type='text' placeholder={t('input_answer')} />
             </S.Blackboard>
           </S.InputSection>
         </S.Container>
