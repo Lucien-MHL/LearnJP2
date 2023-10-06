5fa0c891fcb1cb4550570243b9dc208c5618ae14
Lucien <lucienengineerlife@gmail.com>
2023-10-06 16:40:27 +0800

    feat: web view finish UI

---
+++ src/components/GamePage/WebView/Finish/index.jsx
@@ -0,0 +1,57 @@
+import { useTranslation } from 'react-i18next'
+import { useTheme } from 'styled-components'
+import { useParams } from 'react-router-dom'
+import { useMemo } from 'react'
+import { S } from './styles'
+import data from '../../../../assets/data'
+
+export default function Finish() {
+  const { t } = useTranslation()
+  const { red, green } = useTheme()
+  const { category } = useParams()
+  /** TODO:
+   *    為了檢測畫面是否可行，所以只先回傳字串，
+   *    等撰寫邏輯時，再把回傳值做更改。
+   */
+  const linkList = useMemo(() => {
+    const getOthers = Object.keys(data).filter(i => i !== category)
+    const context = [
+      t('back_to_home'),
+      ...getOthers.map(i => t('go_challenge', { name: t(i) })),
+    ]
+    return context
+  }, [category, t])
+  /** TODO:
+   *    暫時的假資料，用以測試畫面。
+   *    等撰寫此業的邏輯時，即可刪除。
+   */
+  const fakeData = [
+    { fail: 'aws', word: 'ア', sound: 'a', id: '1dc2c4d690ee8bdf' },
+    { fail: 'aws', word: 'イ', sound: 'i', id: '60489b5f9cb0ee19' },
+    { fail: 'aws', word: 'ウ', sound: 'u', id: 'c558111f32794060' },
+    { fail: 'aws', word: 'エ', sound: 'e', id: 'fdbe0e8dadf9b900' },
+    { fail: 'aws', word: 'オ', sound: 'o', id: 'b3bd5993aaf52bba' },
+  ]
+
+  return (
+    <>
+      <S.Header>
+        <S.Title>{t('finish.well_done')}</S.Title>
+      </S.Header>
+      <S.FailingDisplay>
+        {fakeData.map(i => (
+          <S.Row key={i.id}>
+            <S.Item>{i.word}</S.Item>
+            <S.Item $color={green}>{i.sound}</S.Item>
+            <S.Item $color={red}>{i.fail}</S.Item>
+          </S.Row>
+        ))}
+      </S.FailingDisplay>
+      <S.LinkGroup>
+        {linkList.map(e => (
+          <S.link key={e}>{e}</S.link>
+        ))}
+      </S.LinkGroup>
+    </>
+  )
+}
---
+++ src/components/GamePage/WebView/Finish/styles.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const S = {
+  Header: styled.header`
+    display: flex;
+    justify-content: center;
+    padding: 50px;
+  `,
+  Title: styled.p`
+    color: ${({ theme }) => theme.mine_shaft};
+    font-size: 64px;
+    font-weight: bold;
+  `,
+  FailingDisplay: styled.section`
+    @font-face {
+      font-family: Chalk;
+      src: url('./font/Chalk.ttf');
+    }
+
+    width: 60%;
+    margin: auto;
+    background-color: ${({ theme }) => theme.dark_green};
+    border-width: 7px 11px;
+    border-style: solid;
+    border-color: ${({ theme }) => theme.brown};
+    border-radius: 7px;
+    box-shadow: ${({ theme }) => theme.mine_shaft} 2px 2px 6px 0px inset,
+      ${({ theme }) => theme.mine_shaft} -2px -2px 6px 1px inset;
+
+    & > * {
+      color: ${({ theme }) => theme.mercury};
+      font-family: Chalk;
+    }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 4px 0;
+    user-select: none;
+  `,
+  Row: styled.div`
+    display: flex;
+    width: 100%;
+    padding: 8px 0;
+  `,
+  Item: styled.p`
+    width: 33%;
+    text-align: center;
+    font-size: 36px;
+    color: ${({ $color, theme }) => ($color ? $color : theme.mercury)};
+
+    &:nth-child(1) {
+      font-size: 48px;
+    }
+  `,
+  LinkGroup: styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* TODO: 
+         如果有錯誤的題目，則 margin-top 為 85px，
+         其餘則為 auto。
+     */
+    margin-top: auto;
+    height: 35%;
+  `,
+  link: styled(Link)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 110px;
+    height: 230px;
+    margin: 0 25px;
+    text-decoration: none;
+    writing-mode: vertical-lr;
+    color: ${({ theme }) => theme.mine_shaft};
+    font-size: 22px;
+    user-select: none;
+    text-align: center;
+    transition: translate 0.2s ease-in-out;
+
+    &:nth-child(1) {
+      font-size: 44px;
+    }
+
+    &::after {
+      content: '';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      margin: auto;
+      z-index: -1;
+      background: url('./lantern.png');
+      background-position: center;
+      background-size: cover;
+      background-repeat: no-repeat;
+    }
+
+    &:hover {
+      translate: 0 -10px;
+    }
+  `,
+}
